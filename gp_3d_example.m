c7883268789278661e88d7d8d451bf0aed1de19b
marija-p <mpopovic@ethz.ch>
2018-07-03 22:00:52 +0200

    Add CMAES + polynomial planning libraries.

---
+++ gp_3d_example.m
@@ -0,0 +1,65 @@
+load ground_truth.mat
+
+%% Hyperparameter training %%
+
+% Training data.
+X_train = mesh;
+Y_train = ground_truth;
+
+train_hyperparameters = 1;
+
+inf_func = @infExact;
+cov_func = @covSEiso; 
+lik_func = @likGauss; sn = 0.1; hyp.lik = log(sn);
+
+if (train_hyperparameters)
+    hyp.cov = [0 ; 0];
+    hyp.lik = log(0.1);
+    hyp_trained = ...
+        minimize(hyp, @gp, -200, inf_func, [], cov_func, lik_func, X_train, Y_train);
+end
+
+%% GP Regression %%
+X_predict = X_train;
+ind_test = datasample(1:size(X_predict,1),10);
+X_test = X_train(ind_test,:);
+Y_test = Y_train(ind_test);
+
+% ymu, ys: mean and covariance for output
+% fmu, fs: mean and covariance for latent variables
+% post: struct representation of the (approximate) posterior
+[ymu, ys, fmu, fs, ~ , post] = ...
+    gp(hyp_trained, inf_func, [], cov_func, lik_func, ...
+    X_test, Y_test, X_predict);
+Y_predict = ymu;
+
+%% Plotting %%
+subplot(1,4,1)
+scatter3(X_train(:,1), X_train(:,2), X_train(:,3), 100, Y_train, 'filled');
+title('Training data (GT)')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+caxis([0 45])
+subplot(1,4,2)
+scatter3(X_test(:,1), X_test(:,2), X_test(:,3), 100, Y_test, 'filled');
+title('Test (input)')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+caxis([0 45])
+subplot(1,4,3)
+scatter3(X_predict(:,1), X_predict(:,2), X_predict(:,3), 100, Y_predict, 'filled');
+title('Prediction')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+caxis([0 45])
+subplot(1,4,4)
+scatter3(X_predict(:,1), X_predict(:,2), X_predict(:,3), 100, ys, 'filled');
+title('Variance')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+set(gcf, 'Position', [224, 639, 1021, 332]);
