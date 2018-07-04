52c1bdad58da6679d593a4dab78adc1fb5289ec3
marija-p <mpopovic@ethz.ch>
2018-07-04 09:06:47 +0200

    Plotting greedy planner results.

--- greedy_planning_example.m
+++ greedy_planning_example.m
@@ -17,6 +17,21 @@ for i = 1:num_iters
     % Find most uncertain point.
     [~, point_next_ind] = max(ys);
     
+    subplot(1,num_iters,i)
+    hold on
+    scatter3(X_predict(:,1), X_predict(:,2), X_predict(:,3), 100, ymu, 'filled');
+    scatter3(X_train(point_next_ind,1), X_train(point_next_ind,2), ...
+        X_train(point_next_ind,3), Y_train(point_next_ind), 200, 'r', 'filled')
+    title(['[', num2str(X_train(point_next_ind,1),4), ', ', ...
+        num2str(X_train(point_next_ind,2),4), ', ', ...
+        num2str(X_train(point_next_ind,3),4), ']'])
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    caxis([0 45])
+    view(3)
+    hold off
+    
     % Update test set with this point.
     X_test = [X_test; X_train(point_next_ind, :)];
     Y_test = [Y_test; Y_train(point_next_ind)];
@@ -26,17 +41,6 @@ for i = 1:num_iters
         gp(hyp_trained, inf_func, [], cov_func, lik_func, ...
         X_test, Y_test, X_predict);
     
-    subplot(1,num_iters,i)
-    hold on
-    scatter3(X_predict(:,1), X_predict(:,2), X_predict(:,3), 100, ymu, 'filled');
-    scatter3(X_train(point_next_ind,1), X_train(point_next_ind,2), ...
-        X_train(point_next_ind,3), Y_train(point_next_ind), 200, 'k')
-    title(['Point ', num2str(i)])
-    xlabel('x')
-    ylabel('y')
-    zlabel('z')
-    caxis([0 45])
-    view(3)
-    hold off
-    
 end
+
+set(gcf, 'Position', [58, 690, 1182, 281])
