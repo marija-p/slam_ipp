74c92fe25187807ad15b4f8b639c08c45667ab11
marija-p <mpopovic@ethz.ch>
2018-10-25 13:54:12 +0200

    Add modified kernel function in 3D.

--- add_dirs_to_path.m
+++ add_dirs_to_path.m
@@ -3,5 +3,6 @@ addpath(genpath('lib/random_field'), genpath('lib/polynomial_planning'), ...
     genpath('lib/gpml-matlab-v3.5'), genpath('lib/cmaes'))
 
 addpath(genpath('gp_2d_slamtb_graph'))
-
 addpath(genpath('gp_3d_slamtb_graph'))
+
+addpath(genpath('tools'))
