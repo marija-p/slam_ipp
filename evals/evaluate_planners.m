18dda78fd2b3d8755a78b21681f2a56fc60b69f4
marija-p <mpopovic@ethz.ch>
2018-11-18 14:03:21 +0100

    Make added noise in both + and - directions.

--- evals/evaluate_planners.m
+++ evals/evaluate_planners.m
@@ -13,23 +13,24 @@ logger = [];
 
 for t = 1:num_trials
     
- %   planning_params.control_noise_percent = [10, 10, 10];
+%   planning_params.control_noise_percent = [10, 10, 10];
     
-    rng(t, 'twister');
     logger.(['trial', num2str(t)]).num = t;
     
+    rng(t, 'twister');
     gp_params.use_modified_kernel = 0;
     [metrics] = slam_gp(map_params, planning_params, opt_params, gp_params, ...
         training_data, gt_data, testing_data);
     logger.(['trial', num2str(t)]).('no_UI') = metrics;
     clear global
     
+    rng(t, 'twister');
     gp_params.use_modified_kernel = 1;
     [metrics] = slam_gp(map_params, planning_params, opt_params, gp_params, ...
         training_data, gt_data, testing_data);
     logger.(['trial', num2str(t)]).('UI') = metrics; 
     clear global
     
-    disp(['Completed Trial', num2str(t)])
+    disp(['Completed Trial ', num2str(t)])
     
 end
