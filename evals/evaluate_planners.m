a8500b141ad04d69e86a2dff4c7d9b5cd2c7ea6b
marija-p <mpopovic@ethz.ch>
2018-11-19 07:47:44 +0100

    Add first results with variable percent noise model.

--- evals/evaluate_planners.m
+++ evals/evaluate_planners.m
@@ -1,4 +1,21 @@
-num_trials = 5;
+%clear all; close all; clc;
+
+% If data already exists, want to append to it for the trials it contains.
+append_to_logger = 1;
+
+% Number of trials to run
+if (~append_to_logger)
+    num_trials = 5;
+else
+    trials = fieldnames(logger);
+    trials = regexp(trials,'\d*','Match');
+    trials = [trials{:}];
+    trials_names = [];
+    for i = 1:length(trials)
+        trials_names = ...
+            [trials_names; str2num(cell2mat(trials(i)))];
+    end
+end
 
 % UAV workspace dimensions [m]
 dim_x_env = 12;
@@ -11,17 +28,23 @@ dim_z_env = 5;
 
 logger = [];
 
-for t = 1:num_trials
+for i = 1:5
     
-%   planning_params.control_noise_percent = [10, 10, 10];
+    %   planning_params.control_noise_percent = [10, 10, 10];
+    
+    if (~append_to_logger)
+        t = i;
+    else
+        t = trials_names(i);
+    end
     
     logger.(['trial', num2str(t)]).num = t;
-    
-    rng(t, 'twister');
-    gp_params.use_modified_kernel = 0;
-    [metrics] = slam_gp(map_params, planning_params, opt_params, gp_params, ...
-        training_data, gt_data, testing_data);
-    logger.(['trial', num2str(t)]).('no_UI') = metrics;
+
+%     rng(t, 'twister');
+%     gp_params.use_modified_kernel = 0;
+%     [metrics] = slam_gp(map_params, planning_params, opt_params, gp_params, ...
+%         training_data, gt_data, testing_data);
+%     logger.(['trial', num2str(t)]).('no_UI') = metrics;
     clear global
     
     rng(t, 'twister');
