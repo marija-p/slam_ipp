21d6edbdac990246aead20d7f4068c2c6fefde1c
marija-p <mpopovic@ethz.ch>
2018-11-26 11:29:29 +0100

    Better handling of incomplete trials.

--- evals/plot_metrics_all_sep.m
+++ evals/plot_metrics_all_sep.m
@@ -3,21 +3,15 @@ close all;
 trials = fieldnames(logger);
 methods = fieldnames(logger.trial1);
 
-delete_last_figure = 0;
+% Last trial is incomplete.
+if (length(methods) ~= length(fieldnames(logger.(trials{end}))))
+    trials = trials(1:end-1);
+end
 
 for i = 1:length(trials)
     figure;
     for j = 2:length(methods)
-        if (length(fieldnames(logger.(trials{i}))) == length(methods))
-            plot_metrics(logger.(trials{i}).(methods{j}));
-            legend(methods(2:end));
-        % Incomplete trial.
-        else
-            delete_last_figure = 1;
-        end
+        plot_metrics(logger.(trials{i}).(methods{j}));
+        legend(methods(2:end));
     end
-end
-
-if (delete_last_figure)
-    close;
 end
