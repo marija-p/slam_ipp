bce105e769475388d29f4ce1c1105cccf3372684
Masha <mpopovic@ethz.ch>
2019-02-12 14:33:50 +1100

    Nicer plotting tools.

--- ros/tools/plot_field_map_2d.m
+++ ros/tools/plot_field_map_2d.m
@@ -1,17 +1,23 @@
 figure;
 
 subplot(1,2,1)
-scatter(testing_data.X_test(:,1), testing_data.X_test(:,2), ...
-    200, field_map.mean, 'filled')
+%scatter(testing_data.X_test(:,1), testing_data.X_test(:,2), ...
+%    200, field_map.mean, 'filled')
+imagesc(reshape(field_map.mean, map_params.dim_y, map_params.dim_x));
 caxis([0 50])
-axis([-1, 1, -1, 1])
+%axis([-1, 1, -1, 1])
 colorbar
+set(gca, 'YDir', 'Normal')
+axis off
 title('Mean')
 
 subplot(1,2,2)
-scatter(testing_data.X_test(:,1), testing_data.X_test(:,2), ...
-    200, field_map.cov,'filled')
-axis([-1, 1, -1, 1])
+imagesc(reshape(field_map.cov, map_params.dim_y, map_params.dim_x));
+%axis([-1, 1, -1, 1])
 caxis([0 300])
 colorbar
+set(gca, 'YDir', 'Normal')
+axis off
 title('Covariance')
+
+set(gcf, 'Position', [-468   499   938   376])
