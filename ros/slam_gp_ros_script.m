7043df06429cda204f63a8139c03dfdceb5f6c73
Masha <mpopovic@ethz.ch>
2019-02-14 14:04:52 +1100

    Add transform: link -> scan.

--- ros/slam_gp_ros_script.m
+++ ros/slam_gp_ros_script.m
@@ -9,8 +9,14 @@ transforms.T_LINK_TEMP = ...              % Robot body -> temperature sensor.
      0, 0, 1, 0.122;
      0, 0, 0, 1];
  
-[map_params, planning_params, opt_params, gp_params, training_data, testing_data] = ...
-    load_params_and_data_ros(dim_x_env, dim_y_env);
+transforms.T_LINK_SCAN = ...              % Robot body -> laser scanner.
+     [1, 0, 0, -0.064;
+     0, 1, 0, 0.0;
+     0, 0, 1, 0.122;
+     0, 0, 0, 1];
+ 
+ [map_params, planning_params, opt_params, gp_params, training_data, testing_data] = ...
+     load_params_and_data_ros(dim_x_env, dim_y_env);
 
 % Let's go!
 [metrics] = slam_gp_ros(map_params, planning_params, opt_params, gp_params, ...
